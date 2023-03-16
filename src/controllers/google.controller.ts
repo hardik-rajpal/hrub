0d271249c1c0c16aa3fc05c548bd329db0a7a313
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-03-16 20:43:48 +0530

    id endpoint

--- src/controllers/google.controller.ts
+++ src/controllers/google.controller.ts
@@ -4,7 +4,13 @@ import { NextFunction, Request, Response } from 'express';
 class GoogleController {
   public async getBlogPosts(req: Request, res: Response, next: NextFunction){
     try {
-        res.send(await GoogleService.getBlogs());
+      console.log(req.params)
+        if(req.query.id!==undefined){
+          res.send(await GoogleService.getBlogPostById(req.query.id.toString()))
+        }
+        else{
+          res.send(await GoogleService.getBlogs());
+        }
     } catch (error) {
       next(error);
     }
--- src/services/google.service.ts
+++ src/services/google.service.ts
@@ -14,19 +14,60 @@ const drive = google.drive({
 })
 
 const FOLDER_ID = BLOGS_FOLDER_ID;
-export interface blogPost{
-    title:string;
-    publishedLink:string;
-    docId:string;
-    tags:string[]
-    summary:string;
+export interface blogPost {
+    title: string;
+    publishedLink: string;
+    docId: string;
+    tags: string[]
+    summary: string;
 }
 class GoogleService {
-    private static getPublishedLink(docid:string,embed=true){
-        return `https://docs.google.com/document/d/${docid}/pub`+(embed?'?embedded=true':'');
+    private static getPublishedLink(docid: string, embed = true) {
+        return `https://docs.google.com/document/d/${docid}/pub` + (embed ? '?embedded=true' : '');
+    }
+    private static async getBlogFromFileData(file: any) {
+        const body = {
+            resource: {
+                published: true,
+                publishedOutsideDomain: true,
+                publishAuto: true
+            },
+            fileId: file.id,
+            revisionId: 1
+        };
+        const shortcutId = file.id;
+
+        const params = {
+            fileId: shortcutId,
+            fields: 'shortcutDetails',
+            supportsAllDrives: true,
+        };
+        let summary = '';
+        let tags = [];
+        if (file.description !== undefined && file.description.length > 0) {
+            const metadata = JSON.parse(file.description)
+            summary = metadata.summary;
+            tags = metadata.tags;
+        }
+
+        const blogpost: blogPost = {
+            title: file.name,
+            summary: summary,
+            publishedLink: this.getPublishedLink(file.id),
+            tags: tags,
+            docId: file.id
+        }
+        if (file.mimeType === 'application/vnd.google-apps.shortcut') {
+            const shortcutDetailsResponse = await drive.files.get(params)
+            // console.log(response.data.shortcutDetails)
+            const targetId = shortcutDetailsResponse.data.shortcutDetails.targetId;
+            blogpost.docId = targetId;
+            blogpost.publishedLink = this.getPublishedLink(targetId);
+        }
+        return blogpost;
     }
     public static async getBlogs() {
-        const allBlogPosts:blogPost[] = [];
+        const allBlogPosts: blogPost[] = [];
         const response = await drive.files.list({
             q: `'${FOLDER_ID}' in parents`,
             fields: 'nextPageToken, files(id, name, createdTime, modifiedTime, mimeType, contentHints, description)',
@@ -34,47 +75,18 @@ class GoogleService {
         const files = response.data.files;
         // return;
         for (let file of files) {
-            const body = {
-                resource: {
-                    published: true,
-                    publishedOutsideDomain: true,
-                    publishAuto: true
-                },
-                fileId: file.id,
-                revisionId: 1
-            };
-            const shortcutId = file.id;
-
-            const params = {
-                fileId: shortcutId,
-                fields: 'shortcutDetails',
-                supportsAllDrives: true,
-            };
-            let summary = '';
-            let tags = [];
-            if(file.description!==undefined&&file.description.length>0){
-                const metadata = JSON.parse(file.description)
-                summary = metadata.summary;
-                tags = metadata.tags;
-            }
-            const blogpost:blogPost = {
-                title:file.name,
-                summary:summary,
-                publishedLink:this.getPublishedLink(file.id),
-                tags:tags,
-                docId:file.id
-            }
-            if (file.mimeType === 'application/vnd.google-apps.shortcut') {
-                const shortcutDetailsResponse = await drive.files.get(params)
-                    // console.log(response.data.shortcutDetails)
-                const targetId = shortcutDetailsResponse.data.shortcutDetails.targetId;
-                blogpost.docId = targetId;
-                blogpost.publishedLink = this.getPublishedLink(targetId);
-            }
+            const blogpost = await this.getBlogFromFileData(file);
             allBlogPosts.push(blogpost)
         }
         // console.log(allBlogPosts)
         return allBlogPosts;
+    }
+    public static async getBlogPostById(docId: string) {
+        const response = await drive.files.get({
+            fileId: docId,
+            fields: 'id, name, createdTime, modifiedTime, mimeType, contentHints, description'
+        })
+        return await this.getBlogFromFileData(response.data)
     }
 }
 export default GoogleService;
